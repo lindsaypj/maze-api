d5619278ad4441689594ecbb3ac38f6cf9349b6f
Patrick Lindsay <lindsaypj88@gmail.com>
2024-07-17 21:40:14 -0700

    Fixed typo

--- maze/views.py
+++ maze/views.py
@@ -3,17 +3,18 @@ from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse
 from .models import Maze
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the maze index.")
+  return HttpResponse("Hello, world. You're at the maze index.")
 
 def getMaze(request, width, height):
-    if request.method != "GET":
-      # Validate request params
-      if (width < 4 or width > 100):
-          return HttpResponseBadRequest("Invalid maze width. 4 <= WIDTH <= 100", status=400)
-      if (height < 4 or height > 100):
-          return HttpResponseBadRequest("Invalid maze height. 4 <= HEIGHT <= 100", status=400)
+  if request.method == "GET":
+    # Validate request params
+    if (width < 4 or width > 100):
+        return HttpResponseBadRequest("Invalid maze width. 4 <= WIDTH <= 100", status=400)
+    if (height < 4 or height > 100):
+        return HttpResponseBadRequest("Invalid maze height. 4 <= HEIGHT <= 100", status=400)
 
-      # Generate maze with width * height dimensions
-      newMaze = Maze(width, height)
-      
-      return JsonResponse(newMaze.getMaze(), safe=False)
+    # Generate maze with width * height dimensions
+    newMaze = Maze(width, height)
+    output = newMaze.getMaze()
+    
+    return JsonResponse(output, safe=False)
